74eada72d1725144e0049ca0e5d9ea8382201b96
scottcagno <scottiecagno@gmail.com>
2015-12-31 16:02:00 -0500

    add uuid type and methods to project

---
+++ uuid.go
@@ -0,0 +1,64 @@
+package repono
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"time"
+)
+
+type UUID [16]byte
+
+var cachedNodeId []byte
+
+func init() {
+	cachedNodeId = getNodeId()
+}
+
+func getNodeId() []byte {
+	var d [6]byte
+	inet, err := net.Interfaces()
+	if err == nil {
+		var set bool
+		for _, v := range inet {
+			if len(v.HardwareAddr.String()) != 0 {
+				copy(d[:], []byte(v.HardwareAddr))
+				set = true
+				break
+			}
+		}
+		if set {
+			return d[:]
+		}
+	}
+	rand.Read(d[:])
+	d[0] |= 0x01
+	return d[:]
+}
+
+func NewUUID() *UUID {
+	t := uint64(time.Now().UnixNano()/100 + 0x01b21dd213814000)
+	var b [2]byte
+	rand.Read(b[:])
+	clockSeq := binary.LittleEndian.Uint16(b[:])
+	clockSeq &= 0x3FFF
+	var u UUID
+	binary.LittleEndian.PutUint32(u[0:4], uint32(t&(0x100000000-1)))
+	binary.LittleEndian.PutUint16(u[4:6], uint16((t>>32)&0xFFFF))
+	binary.LittleEndian.PutUint16(u[6:8], uint16((t>>48)&0x0FFF))
+	binary.LittleEndian.PutUint16(u[8:10], clockSeq)
+	copy(u[10:16], cachedNodeId)
+	u[8] &= 0x3F
+	u[8] |= 0x80
+	u[6] = (u[6] & 0x0F) | (0x01 << 4)
+	return &u
+}
+
+func (u *UUID) String() string {
+	return fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+}
+
+func (u *UUID) Bytes() []byte {
+	return u[:]
+}
