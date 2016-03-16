9221888031559a57d6ac47933c703cee0dc7a518
Scott Cagno <scottiecagno@gmail.com>
2016-03-16 17:52:27 -0400

    Update uuid.go

--- uuid.go
+++ uuid.go
@@ -39,13 +39,13 @@ func UUID1() string {
 	t := uint64(time.Now().UnixNano()/100 + 0x01b21dd213814000)
 	var b [2]byte
 	rand.Read(b[:])
-	clockSeq := binary.LittleEndian.Uint16(b[:])
+	clockSeq := binary.BigEndian.Uint16(b[:])
 	clockSeq &= 0x3FFF
 	u := make([]byte, 16)
-	binary.LittleEndian.PutUint32(u[0:4], uint32(t&(0x100000000-1)))
-	binary.LittleEndian.PutUint16(u[4:6], uint16((t>>32)&0xFFFF))
-	binary.LittleEndian.PutUint16(u[6:8], uint16((t>>48)&0x0FFF))
-	binary.LittleEndian.PutUint16(u[8:10], clockSeq)
+	binary.BigEndian.PutUint32(u[0:4], uint32(t&(0x100000000-1)))
+	binary.BigEndian.PutUint16(u[4:6], uint16((t>>32)&0xFFFF))
+	binary.BigEndian.PutUint16(u[6:8], uint16((t>>48)&0x0FFF))
+	binary.BigEndian.PutUint16(u[8:10], clockSeq)
 	copy(u[10:16], cachedNodeId)
 	u[8] &= 0x3F
 	u[8] |= 0x80
